75a622585b4ca4f6c76d18cfb2adf9d773545b9f
Mohammedvahith <vahith2004@gmail.com>
2024-06-28 17:30:11 +0530

    updated

--- Javascript/validation.js
+++ Javascript/validation.js
@@ -18,7 +18,3 @@ $(document).ready(function() {
        }
      })
    });
-window.onload = function() {
-      // Reset the form fields when the page loads
-      document.getElementById("form").reset();
-};
