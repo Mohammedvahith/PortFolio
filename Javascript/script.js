d6a28730e1ac68927441e79d7db26289056cedfc
Mohammedvahith <vahith2004@gmail.com>
2024-07-06 20:43:22 +0530

    updated

--- Javascript/script.js
+++ Javascript/script.js
@@ -38,3 +38,46 @@ window.onload = function() {
       imageElement.addEventListener('mouseleave', handleMouseLeave);
   }
 }
+
+// <!-- tilt js effect starts -->
+VanillaTilt.init(document.querySelectorAll(".tilt"), {
+  max: 15,
+});
+// <!-- tilt js effect ends --> 
+
+/* ===== SCROLL REVEAL ANIMATION ===== */
+const srtop = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 1000,
+  reset: true
+});
+
+/* SCROLL HOME */
+srtop.reveal('.banner .content h1', { delay: 200 });
+srtop.reveal('.banner .content .btn', { delay: 200 });
+
+/* SCROLL ABOUT */
+srtop.reveal('#about .content h3', { delay: 200 });
+srtop.reveal('#about .content .tag', { delay: 200 });
+srtop.reveal('#about .content p', { delay: 200 });
+srtop.reveal('#about .content .box-container', { delay: 200 });
+srtop.reveal('#about .content .btn', { delay: 200 });
+
+/* SCROLL SKILLS */
+srtop.reveal('#skills .container', { interval: 200 });
+srtop.reveal('#skills .container .bar', { delay: 400 });
+
+/* SCROLL EDUCATION 
+srtop.reveal('.education .box', { interval: 200 });
+*/
+/* SCROLL PROJECTS 
+srtop.reveal('.work .box', { interval: 200 });
+*/
+/* SCROLL EXPERIENCE 
+srtop.reveal('.experience .timeline', { delay: 400 });
+srtop.reveal('.experience .timeline .container', { interval: 400 });
+*/
+/* SCROLL CONTACT */
+srtop.reveal('.contact .container', { delay: 400 });
+srtop.reveal('.contact .container .form-group', { delay: 400 });
