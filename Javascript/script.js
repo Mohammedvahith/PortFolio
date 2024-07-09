2759c395776590c9abe97a9ac64a26efb75075ea
Mohammedvahith <vahith2004@gmail.com>
2024-07-09 20:25:52 +0530

    updated

--- Javascript/script.js
+++ Javascript/script.js
@@ -19,30 +19,7 @@ var typed = new Typed(".typing-text", {
   backSpeed: 25,
   backDelay: 500,
 });
-
 // <!-- typed js effect ends -->
-window.onload = function() {
-  const imageElement = document.querySelector('.hoverImage');
-  if (imageElement) {
-      const handleMouseMove = (e) => {
-          let rect = imageElement.getBoundingClientRect();
-          let x = e.clientX - rect.left;
-          let y = e.clientY - rect.top;
-
-          let dx = (x - rect.width / 2) / (rect.width / 2);
-          let dy = (y - rect.height / 2) / (rect.height / 2);
-
-          imageElement.style.transform = `perspective(500px) rotateY(${dx * 5}deg) rotateX(${-dy * 5}deg)`;
-      };
-
-      const handleMouseLeave = () => {
-          imageElement.style.transform = "";
-      };
-
-      imageElement.addEventListener('mousemove', handleMouseMove);
-      imageElement.addEventListener('mouseleave', handleMouseLeave);
-  }
-}
 
 // <!-- tilt js effect starts -->
 VanillaTilt.init(document.querySelectorAll(".tilt"), {
