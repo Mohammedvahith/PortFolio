9c5d6bbabe36cb33177e1ed1af107d838f1ef17d
Mohammedvahith <vahith2004@gmail.com>
2024-07-09 20:55:39 +0530

    updated

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,19 +1,36 @@
 document.addEventListener('visibilitychange', function () {
   const favicon = document.getElementById("icon");
   const logoPath = "../PortFolio/Img/Mcircleperfect.png";
-  
+
   if (document.visibilityState === "visible") {
-      document.title = "Portfolio | Mohammed Vahith";
-      if (favicon) favicon.setAttribute("href", logoPath);
+    document.title = "Portfolio | Mohammed Vahith";
+    if (favicon) favicon.setAttribute("href", logoPath);
   } else {
-      document.title = "Come Back To Portfolio";
-      if (favicon) favicon.setAttribute("href", logoPath);
+    document.title = "Come Back To Portfolio";
+    if (favicon) favicon.setAttribute("href", logoPath);
   }
 });
 
+let sections = document.querySelectorAll('section');
+  let navLinks = document.querySelectorAll('nav a');
+  window.addEventListener('scroll', function() {
+    let top = window.scrollY + 200;
+    sections.forEach(section => {
+      let offset = section.offsetTop;
+      let height = section.offsetHeight;
+      let id = section.getAttribute('id');
+      if (top >= offset && top < offset + height) {
+        navLinks.forEach(link => {
+          link.classList.remove('active');
+        });
+        document.querySelector('nav a[href*=' + id + ']').classList.add('active');
+      }
+    });
+  });
+
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-  strings: ["ml engineer","ai engineer","frontend development", "backend development", "web designing","web development"],
+  strings: ["ml engineer", "ai engineer", "frontend development", "backend development", "web designing", "web development"],
   loop: true,
   typeSpeed: 50,
   backSpeed: 25,
